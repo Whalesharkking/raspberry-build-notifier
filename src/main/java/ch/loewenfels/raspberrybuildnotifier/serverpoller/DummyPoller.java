59415749acd4e8780f5adef84050198924d22a71
sirh3e <sirh3e@users.noreply.github.com>
2018-03-14 17:33:16 +0100

    Update DummyPoller.java

--- src/main/java/ch/loewenfels/raspberrybuildnotifier/serverpoller/DummyPoller.java
+++ src/main/java/ch/loewenfels/raspberrybuildnotifier/serverpoller/DummyPoller.java
@@ -7,11 +7,14 @@ import ch.loewenfels.raspberrybuildnotifier.BuildInformationDto;
 public class DummyPoller extends Poller {
     @Override
     protected Optional<BuildInformationDto> pollLatestBuildState() {
-        //        final BuildInformationDto buildInformationDto = new BuildInformationDto();
-        //        buildInformationDto.setJobName("Dummy-Job");
-        //        buildInformationDto.setJobStatus(JobStatus.FAILURE);
-        //        buildInformationDto.setJobTime(LocalDateTime.now());
-        //        return Optional.of(buildInformationDto);
+        
+        /*
+                final BuildInformationDto buildInformationDto = new BuildInformationDto();
+                buildInformationDto.setJobName("Dummy-Job");
+                buildInformationDto.setJobStatus(JobStatus.FAILURE);
+                buildInformationDto.setJobTime(LocalDateTime.now());
+                return Optional.of(buildInformationDto);
+        */
         return null;
     }
 }
