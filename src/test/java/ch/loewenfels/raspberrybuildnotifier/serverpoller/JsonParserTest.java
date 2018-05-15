5dd17b87f15456b412d6b43655ea398038b6a74f
Elias Christen <Elias.Christen@loewenfels.ch>
2018-05-15 11:22:40 +0200

    Added missing Tests of JsonParser
    Change-Id: Idb52197fcfbf1579a8914a1ce1a2eaf5a45c80cf

--- src/test/java/ch/loewenfels/raspberrybuildnotifier/serverpoller/JsonParserTest.java
+++ src/test/java/ch/loewenfels/raspberrybuildnotifier/serverpoller/JsonParserTest.java
@@ -4,14 +4,17 @@ import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 import org.junit.Test;
 
 import ch.loewenfels.raspberrybuildnotifier.BuildInformationDto;
 
+import com.google.gson.JsonSyntaxException;
+
 public class JsonParserTest {
     @Test
-    public void feature_when_then() {
+    public void jsonParse_whenIsCorrectJson_thenResultMuesBeCorrect() {
         //arrange
         final String testinput =
             "{\"jobName\":\"SampleJobName\",\"jobStatus\":\"SUCCESS\",\"jobTimeLocal\":{\"nano\":0,\"monthValue\":3,\"year\":2018,\"month\":\"MARCH\",\"dayOfMonth\":7,\"dayOfWeek\":\"WEDNESDAY\",\"dayOfYear\":66,\"second\":20,\"minute\":30,\"hour\":8,\"chronology\":{\"calendarType\":\"iso8601\",\"id\":\"ISO\"}}}";
@@ -25,5 +28,61 @@ public class JsonParserTest {
         }
         //assert
         assertThat(result.getJobTime(), equalTo(LocalDateTime.of(2018, 3, 7, 8, 30, 20)));
+    }
+
+    @Test
+    public void jsonParseGet_whenCanNotFindJob_thenResultMuesBeError() {
+        //arrange
+        Optional<BuildInformationDto> optional = null;
+        try {
+            optional = new JsonParser().get();
+        } catch (final Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        //assert
+        assertTrue(optional.isPresent());
+    }
+
+    @Test
+    public void jsonParse_whenIsInvalidJobTimeLocal_thenResultMuesBeNull() throws Exception {
+        //arrange
+        final String testinput =
+            "{\"jobName\":\"SampleJobName\",\"jobStatus\":\"SUCCESS\",\"falseJobTime\":{\"nano\":0,\"monthValue\":3,\"year\":2018,\"month\":\"MARCH\",\"dayOfMonth\":7,\"dayOfWeek\":\"WEDNESDAY\",\"dayOfYear\":66,\"second\":20,\"minute\":30,\"hour\":8,\"chronology\":{\"calendarType\":\"iso8601\",\"id\":\"ISO\"}}}";
+        //act
+        final BuildInformationDto result = new JsonParser().parse(testinput);
+        //assert
+        assertNull(result.getJobTime());
+    }
+
+    @Test
+    public void jsonParse_whenIsInvalidJobName_thenResultMuesBeNull() throws Exception {
+        //arrange
+        final String testinput =
+            "{\"falseJobName\":\"SampleJobName\",\"jobStatus\":\"SUCCESS\",\"jobTimeLocal\":{\"nano\":0,\"monthValue\":3,\"year\":2018,\"month\":\"MARCH\",\"dayOfMonth\":7,\"dayOfWeek\":\"WEDNESDAY\",\"dayOfYear\":66,\"second\":20,\"minute\":30,\"hour\":8,\"chronology\":{\"calendarType\":\"iso8601\",\"id\":\"ISO\"}}}";
+        //act
+        final BuildInformationDto result = new JsonParser().parse(testinput);
+        //assert
+        assertNull(result.getJobName());
+    }
+
+    @Test
+    public void jsonParse_whenIsInvalidJobStatus_thenResultMuesBeNull() throws Exception {
+        //arrange
+        final String testinput =
+            "{\"jobName\":\"SampleJobName\",\"jobStatus\":\"Gegi\",\"jobTimeLocal\":{\"nano\":0,\"monthValue\":3,\"year\":2018,\"month\":\"MARCH\",\"dayOfMonth\":7,\"dayOfWeek\":\"WEDNESDAY\",\"dayOfYear\":66,\"second\":20,\"minute\":30,\"hour\":8,\"chronology\":{\"calendarType\":\"iso8601\",\"id\":\"ISO\"}}}";
+        //act
+        final BuildInformationDto result = new JsonParser().parse(testinput);
+        //assert
+        assertNull(result.getJobStatus());
+    }
+
+    @Test(expected = JsonSyntaxException.class)
+    public void jsonParse_whenIsInvalidJobTimeLocal_thenResultMuesBeError() throws Exception {
+        //arrange
+        final String testinput =
+            "{\"jobName\":\"SampleJobName\",\"jobStatus\":\"SUCCESS\",\"year\":2018,\"month\":\"MARCH\",\"dayOfMonth\":7,\"dayOfWeek\":\"WEDNESDAY\",\"dayOfYear\":66,\"second\":20,\"minute\":30,\"hour\":8,\"chronology\":{\"calendarType\":\"iso8601\",\"id\":\"ISO\"}}}";
+        //act
+        new JsonParser().parse(testinput);
     }
 }
